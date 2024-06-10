1097505df5ba9aebe321a928a8c3eb8ed6f536fb
samuelescavello <scavellosamuele23@gmail.com>
2024-06-10 23:07:14 +0200

    print projects

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <h1 class="text-center">Ciao</h1>
   <i class="fa fa-solid fa-home"></i>
+  <ul>
+    <li v-for="project in projects">{{project.title}}</li>
+  </ul>
 </template>
 
 <script>
+import { store } from './store';
+import axsios from 'axios';
   export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {
+        store,
+        projects:[]
+      }
+    },
+    methods:{
+        getAllProjects(){
+            axsios.get(this.store.apiBaseUrl + '/projects').then((res)=> {
+              // console.log(res.data);
+              this.projects = res.data.results;
+            });
+        }
+    },
+    mounted(){
+        this.getAllProjects();
+    }
   }
 </script>
 
